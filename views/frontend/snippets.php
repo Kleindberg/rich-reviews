ec0e3d6fa1ef0dcb02e4182711b05169380ce5d8
Bogdan <Kleindberg@users.noreply.github.com>
2017-04-30 23:23:36 +0300

    Translation avaible
    Added _e() function to static texts.

--- views/frontend/snippets.php
+++ views/frontend/snippets.php
@@ -1,45 +1,74 @@
 <?php
 	if($data["use_stars"]) {
-		?>
-		<div itemscope itemtype="http://schema.org/Product">
-			<span itemprop="name" style="display:none">
-				<?php echo $data['category']; ?>
-			</span>
-			<?php echo $data['url_markup']; ?>
-			Overall rating:
-			<span itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">
-				<span class="stars">
-					<?php echo $data['stars']; ?>
+		if (isset($stars_only) && $stars_only) {
+			?>
+			<div itemscope itemtype="http://schema.org/<?php if(isset($options['schema_type']) && $options['schema_type'] != '' ) { echo $options['schema_type']; } else { echo 'Product'; } ?>">
+				<span itemprop="name" style="display:none">
+					<?php echo $data['category']; ?>
 				</span>
-				<span class="rating" itemprop="ratingValue" style="display: none !important;">
-					<?php echo $data['average']; ?>
-				</span> based on
-				<span class="votes" itemprop="reviewCount">
-					<?php echo $data['reviewsCount']; ?>
-				</span> reviews
-				<div style="display:none">
-					<span itemprop="bestRating">5</span>
-					<span itemprop="worstRating">1</span>
-				</div>
-			</span>
-		</div>
-		<?php render_custom_styles($data['options']);
+				<?php echo $data['url_markup'];?>
+				<span itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">
+					<span class="stars">
+						<?php echo $data['stars']; ?>
+					</span>
+					<span class="rating" itemprop="ratingValue" style="display: none !important;">
+						<?php echo $data['average']; ?>
+					</span>
+					<span class="votes" itemprop="reviewCount" style="display:none">
+						<?php echo $data['reviewsCount']; ?>
+					</span>
+					<div style="display:none">
+						<span itemprop="bestRating">5</span>
+						<span itemprop="worstRating">1</span>
+					</div>
+				</span>
+			</div>
+			<?php
+		} else {
+			?>
+			<div itemscope itemtype="http://schema.org/<?php if(isset($options['schema_type']) && $options['schema_type'] != '' ) { echo $options['schema_type']; } else { echo 'Product'; } ?>">
+				<span itemprop="name" style="display:none">
+					<?php echo $data['category']; ?>
+				</span>
+				<?php echo $data['url_markup'];
+					_e('Overall rating:' , 'rich-reviews');
+				?>
+				<span itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">
+					<span class="stars">
+						<?php echo $data['stars']; ?>
+					</span>
+					<span class="rating" itemprop="ratingValue" style="display: none !important;">
+						<?php echo $data['average']; ?>
+					</span> <?php _e('based on' , 'rich-reviews'); ?>
+					<span class="votes" itemprop="reviewCount">
+						<?php echo $data['reviewsCount']; ?>
+					</span> <?php _e('reviews' , 'rich-reviews'); ?>.
+					<div style="display:none">
+						<span itemprop="bestRating">5</span>
+						<span itemprop="worstRating">1</span>
+					</div>
+				</span>
+			</div>
+			<?php
+		}
+		render_custom_styles($data['options']);
 	} else {
 		?>
-		<div itemscope itemtype="http://schema.org/Product">
+		<div itemscope itemtype="http://schema.org/<?php if(isset($options['schema_type']) && $options['schema_type'] != '' ) { echo $options['schema_type']; } else { echo 'Product'; } ?>">
 			<span itemprop="name" style="display:none">
 				<?php echo $data['category']; ?>
 			</span>
-			<?php echo $data['url_markup']; ?>
-			Overall rating:
+			<?php echo $data['url_markup'];
+				_e('Overall rating:', 'rich-reviews');
+			?>
 			<span itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">
 				<strong><span class="value" itemprop="ratingValue">
 					<?php echo $data['average']; ?>
-				</span></strong> out of
-				<strong><span itemprop="bestRating">5</span></strong> based on
+				</span></strong> <?php _e('out of' , 'rich-reviews'); ?>
+				<strong><span itemprop="bestRating">5</span></strong> <?php _e('based on' , 'rich-reviews'); ?>
 				<span class="votes" itemprop="reviewCount">
 					<?php echo $data['reviewsCount']; ?>
-				</span> reviews.
+				</span> <?php _e('reviews' , 'rich-reviews'); ?>.
 			</span>
 		</div>
 	<?php
